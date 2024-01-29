ef5fb2087a80e5f99b6020add655ca005413f34f
Natconly <79179892+Natconly@users.noreply.github.com>
2024-01-28 20:35:09 -0600

    Add an if to print if no results

--- main.py
+++ main.py
@@ -22,15 +22,18 @@ job_search_elements = [
     h2_element.parent.parent.parent for h2_element in job_search
 ]
 
-for job_element in job_search_elements:
-    title_element = job_element.find("h2", class_="title")
-    company_element = job_element.find("h3", class_="company")
-    location_element = job_element.find("p", class_="location")
-    print(title_element.text.strip())
-    print(company_element.text.strip())
-    print(location_element.text.strip())
-    links = job_element.find_all("a")
-    for link in links:
-        link_url = link["href"]
-        print(f"Apply here: {link_url}\n")
-    print()
+if len(job_search_elements) == 0:
+    print("No results, try another keyword")
+else:
+    for job_element in job_search_elements:
+        title_element = job_element.find("h2", class_="title")
+        company_element = job_element.find("h3", class_="company")
+        location_element = job_element.find("p", class_="location")
+        print(title_element.text.strip())
+        print(company_element.text.strip())
+        print(location_element.text.strip())
+        links = job_element.find_all("a")
+        for link in links:
+            link_url = link["href"]
+            print(f"Apply here: {link_url}\n")
+        print()
